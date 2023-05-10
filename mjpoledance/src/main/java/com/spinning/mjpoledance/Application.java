9f9fef0a230075dba31923050d020fdf8903819c
Juju <julienkot.c@gmail.com>
2023-05-10 16:38:04 +0200

    Springboot init

---
+++ mjpoledance/src/main/java/com/spinning/mjpoledance/Application.java
@@ -0,0 +1,13 @@
+package com.spinning.mjpoledance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
